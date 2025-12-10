212544e136a277a67788bf1e7e557c76dd0cce66
GabZs77 <gabriel785779@gmail.com>
2025-12-10 01:32:39 -0300

    Criar permission.lua

---
+++ permission.lua
@@ -0,0 +1,21 @@
+    ----------///// SISTEMA-ADMIN /////------------
+    allowedUsers = {
+    ----------///// TIK-TOK /////------------
+    
+    
+    
+    ----------///// TEMPORARIOS /////-------------
+   
+   
+    
+    ----------///// PERMANENTE ACESS /////------------
+    ["Itz_guiix3"] = true,
+    ["Gr3g0rilsir"] = true,
+    ["BlesssdTeam_BT"] = true,
+   
+    ------STAFF
+    ["quiel_noobe"] = true,
+    
+    }
+    
+    return allowedUsers
